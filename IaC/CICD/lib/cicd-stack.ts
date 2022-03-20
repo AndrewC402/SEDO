5918f01c0a45dde986127f6b91e610625fe9dfa7
Andrew <andrewc.london2019a@gmail.com>
2022-03-20 20:35:41 +0000

    added codebuild iac

---
+++ IaC/CICD/lib/cicd-stack.ts
@@ -0,0 +1,27 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import * as cdk from '@aws-cdk/core'
+import { Construct } from 'constructs';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class CicdStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const gitHubSource = codebuild.Source.gitHub ({
+      owner: 'AndrewC402',
+      repo: 'SEDO',
+      webhook: true,
+      webhookFilters: [
+        codebuild.FilterGroup
+          .inEventOf(codebuild.EventAction.PUSH)
+          .andBranchIs('master')
+          .andCommitMessageIs('build')
+      ]
+    })
+
+    new codebuild.Project(this, 'SEDO', {
+      source: gitHubSource
+    })
+  }
+}
